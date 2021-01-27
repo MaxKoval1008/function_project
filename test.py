2cd06e9fd33f427bc103ca20c61c8993cd46ed93
Max Koval <max.koval.1008@gmail.com>
2021-01-27 18:59:22 +0200

    Update Test

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 print('Hello world!')
+
+
+def my_func(a, b):
+    summ = a + b
+    print(f'{a} + {b} = {summ}')
+    return summ
+
+
+my_func(4, 5)
+my_func(6, 7)
+my_func(8, 0)
+
+a = my_func(4, 3)
+print(a)
